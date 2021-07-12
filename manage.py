e2b30f5a42204f708f5be4674d49430b5fbf522b
liaoxingyi <liaoxingyi@douban.com>
2021-07-12 12:09:06 +0800

    pytest and flask blueprint

--- manage.py
+++ manage.py
@@ -2,9 +2,11 @@
 
 from flask.cli import FlaskGroup
 
-from src import app, db
+from src import create_app, db
+from src.api.models import User
 
-cli = FlaskGroup(app)
+app = create_app()
+cli = FlaskGroup(create_app=create_app)
 
 
 @cli.command("recreate_db")
@@ -12,7 +14,6 @@ def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
-    print('♾ 初始化数据库')
 
 
 if __name__ == "__main__":
--- src/__init__.py
+++ src/__init__.py
@@ -3,38 +3,23 @@
 import os
 
 from flask import Flask
-from flask_restx import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-api = Api(app)
-
-app_settings = os.getenv("APP_SETTINGS")
-app.config.from_object(app_settings)
-
 # initiate the db
-db = SQLAlchemy(app)
-
-# import sys
-# print(app.config, file=sys.stderr)
+db = SQLAlchemy()
 
 
-class Ping(Resource):
-    def get(self):
-        return {"status": "success", "message": "pong!"}
+def create_app(script_info=None):
+    app = Flask(__name__)
 
+    app_settings = os.getenv("APP_SETTINGS")
+    app.config.from_object(app_settings)
 
-# model
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
+    db.init_app(app)
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+    from src.api.ping import ping_blueprint
+    @app.shell_context_processor
+    def ctx():
+        return {"db": db, "app": app}
 
-
-api.add_resource(Ping, "/ping")
+    return app
